8594e6f3907e22350c012ddb2bafcd00c69e3d29
Ryuichiro Suzuki <dragon.aka.scooter@gmail.com>
2021-02-15 17:24:56 +0900

    erwrew

--- app/javascript/packs/result.js
+++ app/javascript/packs/result.js
@@ -3,8 +3,8 @@ const definition = document.querySelector('.definition');
 fetch(`https://twinword-word-graph-dictionary.p.rapidapi.com/definition/?entry=${userinput}`, {
   "method": "GET",
   "headers": {
-    "x-rapidapi-key": "3a3fb97506msh8b49abdff28130fp180f5bjsn82a2b01e5bad",
-    "x-rapidapi-host": "twinword-word-graph-dictionary.p.rapidapi.com"
+    "x-rapidapi-key": "",
+    "x-rapidapi-host": ""
   }
 })
 .then(response => response.json())
